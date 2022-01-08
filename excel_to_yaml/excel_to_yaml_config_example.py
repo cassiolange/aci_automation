1af9d63d4124d2f313137d964b2ad54b7d6acdd9
Cassio Lange <calange@cisco.com>
2022-01-08 19:24:55 +0000

    add excel to yaml config example

---
+++ excel_to_yaml/excel_to_yaml_config_example.py
@@ -0,0 +1,10 @@
+config = [
+    {
+        'excel' : 'aci_build_input_data.xlsx',
+        'output_dir' : '../aci_automation/aci'
+    },
+    {
+        'excel': "aci_build_mso.xlsx",
+        'output_dir': '../aci_automation/mso'
+    },
+]
